230c63f19e5f830f2230a4dece375ffe079ed80f
Akshay Patel <ak811226@dal.ca>
2020-11-17 20:37:30 -0400

    Initial Commit

---
+++ src/test/java/com/example/Ecosphere/EcosphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ecosphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
