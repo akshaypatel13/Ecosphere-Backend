4bf183ae6b3c0ed236cac2700fd881782d41e224
Akshay Patel <ak811226@dal.ca>
2020-11-21 21:12:41 -0400

    WebMVCConfiguration

--- src/main/java/com/example/Ecosphere/EcosphereApplication.java
+++ src/main/java/com/example/Ecosphere/EcosphereApplication.java
@@ -5,14 +5,26 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
-@CrossOrigin
 public class EcosphereApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcosphereApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+			}
+		};
 	}
 
 }
