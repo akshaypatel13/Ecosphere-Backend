13f4fee12d4e604845c35dd5832ca0735c05b8b1
akshaypatel13 <pakshay802@gmail.com>
2020-12-07 18:28:19 -0400

    Usage & Comments

--- src/main/java/com/example/Ecosphere/chat/chatService.java
+++ src/main/java/com/example/Ecosphere/chat/chatService.java
@@ -14,6 +14,18 @@ public class chatService extends TextWebSocketHandler {
     private final List<WebSocketSession> webSocketSessions = new ArrayList<>();
     private HashSet<TextMessage> Messages = new HashSet<>();
 
+    /*
+    After the successful webSocketRegistration, the particular users are added to the webSocketSession
+    that intends to broadcast the messages to the every single user. The TextWebSocketHandler allows us to
+    broadcast the message to the discussion channel using three Override methods:
+    1. afterConnectionEstablished - this function will add users to the session and will also send previous
+    messages of the discussion forum when the user was not enrolled in.
+    2. handleTextMessage - this function performs the actual communication of the chat messages to the
+    frontEnd part and will also store the messages that can be useful to the new users.
+    3. afterConnectionClosed - this function will remove users from the session so that they don't receive
+    any further chat messages.
+     */
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         webSocketSessions.add(session);
